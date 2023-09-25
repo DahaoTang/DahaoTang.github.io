f44e80b46751644e922e73b4bb58121c048a6de7
Dahao Tang <dahaotang@gmail.com>
2023-09-25 16:25:19 +1000

    header updated

--- app/(site)/components/header/Header.tsx
+++ app/(site)/components/header/Header.tsx
@@ -15,9 +15,9 @@ export default function Header({
 	return (
 		<div className="w-full items-end flex flex-row ">
 			<div className="text-3xl font-bold text-neutral-900 pr-5">{title}</div>
-			{subtitle && <div className="pb-1 pr-1 text-neutral-500">〉〉</div>}
+			{subtitle && <div className="pb-1 pr-1 text-neutral-500 text-sm">〉</div>}
 			<div className="text-2xl font-bold text-neutral-700 pr-5">{subtitle}</div>
-			{subsubtitle && <div className="pb-1 pr-1 text-neutral-500">〉〉</div>}
+			{subsubtitle && <div className="pb-1 pr-1 text-neutral-500 text-sm">〉</div>}
 			<div className="text-2xl text-sky-700">{subsubtitle}</div>
 		</div>
 	);
