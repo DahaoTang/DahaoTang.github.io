3ff7af554c9953671baadf65b11a06f7f6eacc63
Dahao Tang <dahaotang@gmail.com>
2023-09-25 17:13:29 +1000

    post contents updated

--- app/(site)/projects/personal/202310_1/page.tsx
+++ app/(site)/projects/personal/202310_1/page.tsx
@@ -1,13 +1,14 @@
 "use client";
 
-import { Heading1 } from "@/app/(site)/components/Heading";
+import { Heading1, Heading2 } from "@/app/(site)/components/Heading";
 import Header from "@/app/(site)/components/header/Header";
 
 export default function PersonalProject202310_1() {
 	return (
 		<div>
 			<Header title="Personal Project" subtitle="Prompt Engineering" />
-			<Heading1 content="Introduction" />
+			{/* <Heading1 content="Introduction" /> */}
+			<Heading2 content="Coming soon..." />
 		</div>
 	);
 }
