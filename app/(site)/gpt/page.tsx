c42c32be3ebba0b44eb70f69807d595cdb4bf4ec
Dahao Tang <dahaotang@gmail.com>
2023-10-07 03:45:27 +1100

    poem updated

---
+++ app/(site)/gpt/page.tsx
@@ -0,0 +1,10 @@
+"use client";
+import Header from "@/app/(site)/components/header/Header";
+
+export default function GPT() {
+	return (
+		<div>
+			<Header title="GPT Test" />
+		</div>
+	);
+}
--- app/(site)/poems/Poems.ts
+++ app/(site)/poems/Poems.ts
@@ -1,4 +1,7 @@
 import { poem20230930_1 } from "./20230930_1/Summary";
 import { poem20230910_1 } from "./20230910_1/Summary";
 
-export const poems = [poem20230930_1, poem20230910_1];
+export const poems = [
+	// poem20230930_1, 
+	poem20230910_1
+];
