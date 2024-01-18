6add5f260d6f05237c2db22eb24a047e08a41144
Dahao Tang <dahaotang@gmail.com>
2024-01-18 15:44:29 +1100

    fun project index updated

--- app/(site)/projects/fun/Projects.ts
+++ app/(site)/projects/fun/Projects.ts
@@ -4,7 +4,7 @@ import { funProject202312_1 } from "./202312-1/Summary";
 import { funProject202401_1 } from "./202401-1/Summary";
 
 export const projects = [
-	funProject202401_1,
+	// funProject202401_1,
 	funProject202312_1,
 	funProject202310_1,
 	funProject202309_1,
