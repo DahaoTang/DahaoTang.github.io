a49f3b56680e030d5ff85f54bfc190042fe3bda2
Dahao Tang <dahaotang@gmail.com>
2024-05-21 16:45:24 +1000

    post updated

--- app/(site)/posts/20230925-1/Summary.ts
+++
@@ -1,7 +0,0 @@
-export const post20230925_1 = {
-	id: "20230925-1", // "YYYYMMDD-No."
-	date: "2023.9.25",
-	title: "Test Post",
-	keyWords: "Web; Next.js",
-	topic: "Web",
-};
--- app/(site)/posts/Posts.ts
+++ app/(site)/posts/Posts.ts
@@ -1,4 +1,3 @@
-import { post20230925_1 } from "./20230925-1/Summary";
 import { post20240521_1 } from "./20240521-1/Summary";
 
-export const posts = [post20240521_1, post20230925_1];
+export const posts = [post20240521_1];
