cf25bdebf5d5eed777310492889a530992f2baac
Dahao Tang <dahaotang@gmail.com>
2024-11-13 14:30:56 +1100

    updated

--- app/(site)/contact/page.tsx
+++ app/(site)/contact/page.tsx
@@ -12,20 +12,32 @@ export default function Contact() {
 		<div>
 			<Heading1 content="Contact" />
 			<div className="w-[100%] flex flex-col py-10 space-y-10">
-				<DefaultButton
-					label="LinkedIn"
-					href="https://www.linkedin.com/in/dahaotang/"
-					target="_blank"
-					rel="noopener noreferrer"
-					additionalButtonClasses="hover:outline-rose-500 hover:bg-rose-500"
-				/>
-				<DefaultButton
-					label="Github"
-					href="https://github.com/DahaoTang"
-					target="_blank"
-					rel="noopener noreferrer"
-					additionalButtonClasses="hover:outline-rose-500 hover:bg-rose-500"
-				/>
+				<button
+					className={
+						"px-3 py-2 outline outline-neutral-900 hover:bg-neutral-900 hover:text-rose-300"
+					}
+				>
+					<a
+						href="https://www.linkedin.com/in/dahaotang/"
+						target="_blank"
+						rel="noopener noreferrer"
+					>
+						LinkedIn
+					</a>
+				</button>
+				<button
+					className={
+						"px-3 py-2 outline outline-neutral-900 hover:bg-neutral-900 hover:text-rose-300"
+					}
+				>
+					<a
+						href="https://github.com/DahaoTang"
+						target="_blank"
+						rel="noopener noreferrer"
+					>
+						GitHub
+					</a>
+				</button>
 				<div className="flex items-center">
 					<button
 						onClick={() => {
@@ -39,7 +51,7 @@ export default function Contact() {
 								}
 							);
 						}}
-						className="w-full px-3 py-2 outline outline-neutral-900 hover:outline-sky-500 hover:bg-sky-500 hover:text-white"
+						className="w-full px-3 py-2 outline outline-neutral-900  hover:bg-neutral-900 hover:text-green-300"
 					>
 						{copyUniEmailSuccess && <div>Uni email copied!</div>}
 						{!copyUniEmailSuccess && <div>dahao.tang@sydney.edu.au</div>}
@@ -58,7 +70,7 @@ export default function Contact() {
 								}
 							);
 						}}
-						className="w-full px-3 py-2 outline outline-neutral-900 hover:outline-sky-500 hover:bg-sky-500 hover:text-white"
+						className="w-full px-3 py-2 outline outline-neutral-900  hover:bg-neutral-900 hover:text-green-300"
 					>
 						{copyGmailSuccess && <div>Gmail copied!</div>}
 						{!copyGmailSuccess && <div>dahaotang.work@gmail.com</div>}
