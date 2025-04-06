1dc8fb61c44eefa2bfbeb89e1e0b433deb115c6e
Dahao Tang <dahaotang@gmail.com>
2025-04-07 05:16:02 +1000

    layout file updated

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,22 +3,9 @@ import { Analytics } from "@vercel/analytics/react";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 
 import "./globals.css";
-import { Roboto } from "next/font/google";
 import { Noto_Sans } from "next/font/google";
-import { Outfit } from "next/font/google";
-
-const roboto = Roboto({
-  weight: "400",
-  subsets: ["latin"],
-  display: "swap",
-});
 
 const noto_sans = Noto_Sans({
-  subsets: ["latin"],
-  display: "swap",
-});
-
-const outfit = Outfit({
   subsets: ["latin"],
   display: "swap",
 });
