2e90a7006af247d6f88e6e673e2e69a46a2ad145
Dahao Tang <dahaotang@gmail.com>
2025-04-07 06:10:14 +1000

    education updated

--- app/components/education.tsx
+++ app/components/education.tsx
@@ -12,9 +12,6 @@ export default function Education() {
           <div>Bachelor of Commerce, majoring in Finance</div>
         </div>
       </div>
-      {/* <div><span className="pr-1 text-neutral-500">2016 - 2019</span> Nanjing No.13 High School</div>
-      <div><span className="pr-1 text-neutral-500">2013 - 2016</span> Nanjing No.1 Middle School</div>
-      <div><span className="pr-1 text-neutral-500">2007 - 2013</span> Nanjing Xiaoxihu Primary School</div> */}
     </div>
   );
 }
